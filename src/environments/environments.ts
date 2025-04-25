d4abc22dfdb886ad8c91a4757b5c6201f40901e6
claytolovin <luvinstart@gmail.com>
2025-04-25 20:10:13 -0300

    adjusting the url

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    //apiUrl: 'https://petalflow.com.br/api'
-    apiUrl: 'http://127.0.0.1:8000/api'
+    apiUrl: 'https://petalflow.com.br/api'
+    //apiUrl: 'http://127.0.0.1:8000/api'
   };
   
